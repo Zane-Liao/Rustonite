5f1ce73a32bd050d737cbca5f70181a78783a03b
Zhengqiang Liao <lzq666amn@gmail.com>
2025-05-30 19:54:13 +0800

    Initial

---
+++ core/src/lib.rs
@@ -0,0 +1,25 @@
+//! Rust 2024 !!!
+
+pub mod market_data {
+    pub mod traits;
+    pub mod websockets;
+    pub mod process;
+    pub mod transfor;
+    pub mod boardcaster;
+}
+
+pub mod execution {
+    pub mod traits;
+}
+
+pub mod risk {
+    pub mod traits;
+}
+
+pub mod storage {
+    pub mod traits;
+}
+
+pub mod utils {
+    pub mod traits;
+}
---
+++ core/src/market_data/websockets.rs
@@ -0,0 +1,31 @@
+//!
+//! 
+//! 
+//! 
+//! 
+
+
+use std::path::Display;
+use std::path::Path;
+use std::io::*;
+use std::collections::BTreeMap;
+use std::collections::HashMap;
+use tokio::net::TcpStream;
+
+
+
+
+
+#[tokio::main]
+async fn main() {
+    let url = "";
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn client() {}
+}
